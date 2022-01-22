4a53afffa4f805e185169e343c1b5e10f2836b28
Begli0102 <begli.kaka@mail.ru>
2022-01-22 22:45:54 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 function App() {
+const firstRender = useRef(true);
+
+const [inputValue, setInputValue] = useState('');
+const [todos, setToDos] = useState([]);
+
+const handleAdd=(e)=>{
+  e.preventDefault(e);
+ 
+  if(inputValue.trim() ==='') return;
+ 
+  setToDos([...todos,{
+    text:inputValue,
+    id:uuidv4()
+  }])
+ 
+  setInputValue('');
+}
+
+const handleDelete=(id)=>{
+  setToDos(todos.filter((todo)=>todo.id !==id))
+} 
+
+useEffect(()=>{
+if(firstRender.current){
+
+  firstRender.current=false;
+}else{
+  localStorage.setItem('Todo', JSON.stringify([...todos]))
+}
+
+}, [todos]);
+
+useEffect(()=>{
+
+if(localStorage.getItem('Todo') !==null){
+  const newTodos = localStorage.getItem('Todo');
+  setToDos(JSON.parse([...todos,newTodos]))
+}
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <form onSubmit={handleAdd}>
+          <input type="text" 
+          autoFocus
+          value = {inputValue}
+          onChange={e=>setInputValue(e.target.value)}
+          placeholder='Add todo...'
+          />
+          <button type='submit'>Add</button>
+        </form>
+        {todos.map((todo)=>(
+          <div key ={todo.id} className="todo">
+            <p>{todo.text} </p>
+            <i onClick={()=> handleDelete(todo.id)} className="fas fa-trash-alt"></i>
+        
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
